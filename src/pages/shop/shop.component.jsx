dff16bcf1ab597039d23aead6c03c7335eac8ef6
Vimal Jaiswal <jaiswalvimal1@gmail.com>
2020-10-07 20:08:58 +0530

    updating the shop page with compoenent preview and component item

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+import SHOP_DATA from "./shop.data";
+
+import CollectionPreview from "../../components/collection-preview/collection-preview";
+
+const ShopPage = () => {
+  const [collections, setCollections] = useState(SHOP_DATA);
+
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+export default ShopPage;
